4dff603717e58a018c80d20d8fa981e8271730f6
Coin-233 <coin233mail@gmail.com>
2024-06-29 17:42:38 +0800

    5

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,6 +1,9 @@
 export default {
   items: [
-    "pic/example1.jpg",
-    "pic/example2.jpg"
+    "pic/Dreams_3.jpeg",
+    "pic/Dreams_4.jpeg",
+    "pic/Arnoia_5.png",
+    "pic/ES_Beach_3.jpg",
+    "pic/ES_Beach_4.jpg",
   ]
 }
